b1cc25bdaa1428767b64e81f0f540e7bf36991ce
Michael Ward <michael.ward@keyin.com>
2025-04-14 15:25:59 -0230

    5 java files added

---
+++ Member.java
@@ -0,0 +1,65 @@
+public class Member extends User {
+    public Member() {
+        this.role = "Member";
+    }
+
+    public Member(String username, String password, String email, String phoneNumber, String address, String role){
+        super(username, password, email, phoneNumber, address, "Member");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
